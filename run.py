ae670b1990da8eacba627cbbbe59aec5c7aa5bbe
Onii Chan <46487694+sogood99@users.noreply.github.com>
2025-02-21 04:05:40 -0500

    Add simplistic data recording and prediction handling using Myo and KNN classifier (untested)

---
+++ run.py
@@ -0,0 +1,80 @@
+# Simplistic data recording
+import time
+import multiprocessing
+import numpy as np
+from data import Label
+from knn import Classifier
+import torch
+
+from pyomyo import Myo, emg_mode
+
+from pynput import keyboard, mouse
+
+
+def handle_prediction(pred):
+    if pred == Label.Fist:
+        mouse.Controller().move(-10, 0)
+    elif pred == Label.Paper:
+        mouse.Controller().move(10, 0)
+    elif pred == Label.HalfHeart:
+        mouse.Controller().move(0, -10)
+    elif pred == Label.Zero:
+        mouse.Controller().move(0, 10)
+    elif pred == Label.Gun:
+        mouse.Controller().click(mouse.Button.left)
+
+
+def run_predictor(seconds, mode):
+    """
+    Run the predictor for a given number of seconds
+    """
+
+    cls = Classifier(
+        np.load("data/samples.npy"),
+        np.load("data/labels.npy").squeeze(),
+        torch.load("model.pth")[0][:3],
+    )
+
+    m = Myo(mode=mode)
+    m.connect()
+
+    myo_data = []
+
+    def add_to_queue(emg, movement):
+        myo_data.append(emg)
+
+    m.add_emg_handler(add_to_queue)
+
+    def print_battery(bat):
+        print("Battery level:", bat)
+
+    m.add_battery_handler(print_battery)
+
+    m.set_leds([0, 128, 0], [0, 128, 0])
+    m.vibrate(1)
+
+    print("Started running predictor")
+    start_time = time.time()
+
+    while True:
+        if time.time() - start_time < seconds:
+            m.run()
+            if len(myo_data) > 25:
+                data = np.array(myo_data[:25])
+                data = data.reshape(1, 25, 8)
+                data = data.mean(axis=1)
+                data = data.reshape(1, -1)
+                pred = cls.predict(data)[0]
+                if pred != Label.Unknown:
+                    print(pred)
+                    handle_prediction(pred)
+                myo_data = myo_data[25:]
+
+        else:
+            break
+
+
+if __name__ == "__main__":
+    seconds = 0.5
+    mode = emg_mode.PREPROCESSED
+    run_predictor(seconds, mode)
